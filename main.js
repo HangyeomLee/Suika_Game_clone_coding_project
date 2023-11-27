cb8d2d0fc9955dbc71256520d579d3629f7e120b
Hangyeom Lee <gksrual0813@gmail.com>
2023-11-26 22:32:26 -0500

    add fruit picture

--- main.js
+++ main.js
@@ -1,24 +1,40 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+import {Bodies, Engine, Render, Runner, World} from "matter-js";
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const engine = Engine.create();
+const render = Render.create({
+        engine,
+        element: document.body,
+        options: {
+            wireframes: false,
+            background: "#F7F4C8",
+            width: 620,
+            height: 850,
+    }
+});
 
-setupCounter(document.querySelector('#counter'))
+const world = engine.world;
+
+const leftWall = Bodies.rectangle(15,395,30,790,{
+    isStatic: true,
+    render:{fillStyle: "#E6B143"}
+});
+
+const rightWall = Bodies.rectangle(605,395,30,790,{
+    isStatic: true,
+    render:{fillStyle: "#E6B143"}
+});
+
+const groundWall = Bodies.rectangle(310,820,620,60,{
+    isStatic: true,
+    render:{fillStyle: "#E6B143"}
+});
+
+const topLine = Bodies.rectangle(310, 150, 620, 2,{
+    isStatic: true,
+    render:{fillStyle: "#E6B143"}
+});
+
+World.add(world, [leftWall, rightWall, groundWall,topLine]);
+
+Render.run(render);
+Runner.run(engine);
